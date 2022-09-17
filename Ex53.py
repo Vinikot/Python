621776ee043a82abeea618f55ce2480c0b8b97c8
ViniKot <vini.kotches@gmail.com>
2022-09-16 22:44:28 -0300

    Arrays,complete

--- Ex53.py
+++ Ex53.py
@@ -1,5 +1,6 @@
 num=[]
+c=int(input("Digite um valor para a constante: "))
 for i in range(0,20,1):
     num.append(float(input("Digite um valor qualquer: ")))
-    num[i]=num[i]*10
+    num[i]=num[i]*c
     print(num[i])
--- Ex54.py
+++ Ex54.py
@@ -1,7 +1,8 @@
 num=[]
 num2=[]
+c=int(input("Digite um valor para a constante: "))
 for i in range(0,20,1):
     num.append(float(input("Digite um valor qualquer: ")))
-    num2.append(num[i]*10)
+    num2.append(num[i]*c)
 for i in range(0,20,1):
     print(num[i],num2[i])
