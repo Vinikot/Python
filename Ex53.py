65c9c96f848a560f0e62aa3d38bc9988690cbd68
ViniKot <vini.kotches@gmail.com>
2022-09-16 23:06:14 -0300

    Arrays,complete

--- Ex53.py
+++ Ex53.py
@@ -3,4 +3,5 @@ c=int(input("Digite um valor para a constante: "))
 for i in range(0,20,1):
     num.append(float(input("Digite um valor qualquer: ")))
     num[i]=num[i]*c
-    print(num[i])
+for i in num:
+    print(i)
